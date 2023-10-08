b243052599b8a70ee3e7f90229c2f0602182b509
helmy162 <104876574+helmy162@users.noreply.github.com>
2023-10-08 19:41:51 +0300

    Refactored Existing Code

--- Assignment.cpp
+++ Assignment.cpp
@@ -13,15 +13,57 @@ using namespace std;
     After finishing, you are requested to write unite tests below in the main code for the function.
 */
 
-int sum (int arr[], int s, int e, int n) {  
-    int x = 0;  
-    for (int i = s; i < e; i++) {  
-        x+=arr[i];
+/**
+ * This function calculates the sum of elements of an array from index `start_index` to `end_index` - 1.
+ * @throw invalid_argument If `start_index` or `end_index` is out of range.
+ */
+int calculateSum(const int arr[], int start_index, int end_index, int arr_size) {
+    // Validate indices
+    if(start_index < 0 || end_index > arr_size || start_index >= end_index) {
+        throw invalid_argument("Invalid index range for summation.");
     }
-    return x;  
+    
+    int sum = 0;  
+    for (int i = start_index; i < end_index; i++) {  
+        sum += arr[i];
+    }
+    return sum;  
 }
 
 int main () {
-    //#TODO Write the unit testing assertions here
-    return 1;
+    // Example array to test the function
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+
+    // Happy Scenarios
+    // Test 1: Normal case
+    assert(calculateSum(arr, 0, 3, arr_size) == 6); // 1 + 2 + 3 = 6
+
+    // Test 2: Entire array
+    assert(calculateSum(arr, 0, arr_size, arr_size) == 21); // 1 + 2 + 3 + 4 + 5 + 6 = 21
+
+    // Test 3: Single element
+    assert(calculateSum(arr, 2, 3, arr_size) == 3); // 3
+
+    // Sad Scenarios
+    // Test 1: Invalid range (start_index >= end_index)
+    try {
+        calculateSum(arr, 3, 3, arr_size);
+        assert(false); // Should not reach here
+    } catch (const invalid_argument&) {
+        assert(true); // Expected to catch exception
+    }
+
+    // Test 2: Out of range
+    try {
+        calculateSum(arr, -1, 4, arr_size);
+        assert(false); // Should not reach here
+    } catch (const invalid_argument&) {
+        assert(true); // Expected to catch exception
+    }
+
+    // If we reached here, so all tests passed
+    cout << "All tests has passed!\n";
+    return 0;
 }
